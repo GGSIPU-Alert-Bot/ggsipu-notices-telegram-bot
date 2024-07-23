70333de7b2add0c4855e065260c8630688ed9b42
shubhsardana29 <shubhsardana29@gmail.com>
2024-07-23 06:35:17 +0530

    chore: Update dependencies and add Express server to check server status

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,33 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const bot_1 = require("./bot");
-const checkNewNotices_1 = require("./bot/actions/checkNewNotices");
-const cron_1 = require("cron");
-const logger_1 = require("./utils/logger");
-const job = new cron_1.CronJob('0 9,12,14,19,21,0 * * *', checkNewNotices_1.checkForNewNotices);
-bot_1.bot.launch();
+const express = require('express');
+const { bot } = require('./bot');
+const { checkForNewNotices } = require('./bot/actions/checkNewNotices');
+const { CronJob } = require('cron');
+const { logger } = require('./utils/logger');
+const app = express();
+const port = process.env.PORT || 8000;
+// Custom API route to check server status
+app.get('/status', (req, res) => {
+    res.json({ message: 'Server is running' });
+});
+// Setting up the cron job
+const job = new CronJob('0 9,12,14,19,21,0 * * *', checkForNewNotices);
+// Launch the bot and start the cron job
+bot.launch();
 job.start();
-logger_1.logger.info('logger outside job');
-// Enable graceful stop
+logger.info('logger outside job');
+// Graceful shutdown
 process.once('SIGINT', () => {
-    bot_1.bot.stop('SIGINT');
+    bot.stop('SIGINT');
     job.stop();
+    process.exit(0);
 });
 process.once('SIGTERM', () => {
-    bot_1.bot.stop('SIGTERM');
+    bot.stop('SIGTERM');
     job.stop();
+    process.exit(0);
+});
+// Start the Express server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
-import { bot } from './bot';
-import { checkForNewNotices } from './bot/actions/checkNewNotices';
-import { CronJob } from 'cron';
-import { logger } from './utils/logger';
+const express = require('express');
+const { bot } = require('./bot');
+const { checkForNewNotices } = require('./bot/actions/checkNewNotices');
+const { CronJob } = require('cron');
+const { logger } = require('./utils/logger');
 
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Custom API route to check server status
+app.get('/status', (req: any, res: { json: (arg0: { message: string; }) => void; }) => {
+  res.json({ message: 'Server is running' });
+});
+
+// Setting up the cron job
 const job = new CronJob('0 9,12,14,19,21,0 * * *', checkForNewNotices);
 
+// Launch the bot and start the cron job
 bot.launch();
 job.start();
 
 logger.info('logger outside job');
 
-// Enable graceful stop
+// Graceful shutdown
 process.once('SIGINT', () => {
   bot.stop('SIGINT');
   job.stop();
+  process.exit(0);
 });
+
 process.once('SIGTERM', () => {
   bot.stop('SIGTERM');
   job.stop();
+  process.exit(0);
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
